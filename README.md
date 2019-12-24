6e329aec18d944e9098d0e108124d912676c8b86
wolf-tungsten <canis.tungsten@foxmail.com>
2019-12-24 14:16:43 +0800

    显示结果的星星

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 ```
 git config --local http.postBuffer 524288000
 ```
+
+## 操作方法
+
+* 按 shift 键启动慢速
+* 按 ⬆️ 键加速
+* 按 Enter 键停止显示结果
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 const AMOUNT = 1000;
-$(function() {
+$(function () {
     //$('cloud').attr('width',document.body.offsetWidth);
     //$('cloud').attr('height',document.body.offsetHeight);
-    $("#cloud-result").hide();  
+    $("#cloud-result").hide();
     let color = ['rgb(207,0,162)', 'rgb(42,222,222)', 'white'];
     function generateTag() {
         let tags = $('#tags');
-        for(let i = 0; i < AMOUNT; i++){
+        for (let i = 0; i < AMOUNT; i++) {
             let tag = $(`<a href="#" id="${i}" class="tag">${i}</div>`)
-            tag.css('color',color[i % 3]);
+            tag.css('color', color[i % 3]);
             tags.append(tag);
         }
     }
@@ -19,71 +19,73 @@ $(function() {
         maxSpeed: 0.05,
         textHeight: 20,
         textColour: null,
-        textFont:'Impact,"Arial Black",sans-serif',
+        textFont: 'Impact,"Arial Black",sans-serif',
         noMouse: true,
         noSelect: true,
-        initial:[-0.002, 0.0001],
+        initial: [-0.002, 0.0001],
         maxSpeed: 1,
-        zoom:  0.9,
+        zoom: 0.9,
         interval: 40,
-        centreImage: 'img/logo.svg', 
-      });
-      TagCanvas.Start('cloud-result', 'tags', {
+        centreImage: 'img/logo.svg',
+    });
+    TagCanvas.Start('cloud-result', 'tags', {
         reverse: true,
         depth: 0.8,
         maxSpeed: 0.05,
         textHeight: 20,
         textColour: null,
-        textFont:'Impact,"Arial Black",sans-serif',
+        textFont: 'Impact,"Arial Black",sans-serif',
         noMouse: true,
         noSelect: true,
-        initial:[0, 0],
+        initial: [0, 0],
         maxSpeed: 1,
-        zoom:  0.9,
+        zoom: 0.9,
         centreImage: 'img/logo.svg',
-        animTiming: 'Linear' 
-      });
-
-    function speedUp(){
+        animTiming: 'Linear'
+    });
+    let result = 0;
+    function random() {
+        result = Math.round(Math.random() * AMOUNT);
+        TagCanvas.TagToFront('cloud-result', {id: ''+result})
+    }
+    function speedUp() {
         let i = 0;
-        let timer = setInterval(()=>{
+        let timer = setInterval(() => {
             if (i < 0.05) {
                 i += 0.005;
-                TagCanvas.SetSpeed('cloud', [-i, i*0.5]);
+                TagCanvas.SetSpeed('cloud', [-i, i * 0.5]);
             } else {
                 console.log('speedUp done')
                 clearInterval(timer);
             }
         }, 100)
     }
-    let current = 0;
-    function stop() {
-        
-        if(current){
-            TagCanvas.Resume('cloud');
-            TagCanvas.SetSpeed('cloud', [-0.002, 0.0001]);
-            $("#cloud").show();
-            $("#cloud-result").hide();      
-        } else {
-            TagCanvas.TagToFront('cloud-result', {id: "114"});
-            TagCanvas.TagToFront('cloud', {id: "114"});
-            $("#cloud").hide();
-            $("#cloud-result").show();
-        }
-        current = !current;
-        //TagCanvas.TagToFront('cloud', {id: "114"});
-        //TagCanvas.TagToFront('cloud', {id: "114"});
-    }
-    function zoomIn(){
 
+    function stop() {
+        $("#cloud").hide();
+        $("#cloud-result").show();
+        TagCanvas.SetSpeed('cloud', [-0.002, 0.0001]);
+        TagCanvas.TagToFront('cloud', { id: ''+result });   
     }
+
+    function resume() {
+        $("#cloud").show();
+        $("#cloud-result").hide();
+        TagCanvas.SetSpeed('cloud', [-0.002, 0.0001]);
+    }
+
     $(document).keydown(function (event) {
         //alert(event.keyCode);
-        if(event.keyCode === 38) {
+        if (event.keyCode === 38) {
             // 按向上键加速
+            random();
             speedUp();
         }
-        if(event.keyCode === 13){
+        if (event.keyCode === 16) {
+            // 按shift键恢复
+            resume();
+        }
+        if (event.keyCode === 13) {
             stop();
         }
     });
